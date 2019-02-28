1d0b87ea40041eff3f7e86403055faa98450b340
Unknown <pcapuano978@west-mec.org>
2019-02-28 15:28:42 -0700

    Project Started; Small Progression
    Not too much time to set anything up, but I did get some stuff started to be ready for a quick start tomorrow.

---
+++ hg_report.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 2
+
+   Author: Patrick M. Capuano
+   Date:  2.28.19
+   
+   Filename: hg_report.js
+	
+*/
+
+var gameReport = "<h1>title</h1>" + "<h2>By: manufacturer</h2>" + "<img src='hg_id.png'" + "alt='id'" + "id='gameImg'/>" + "<table>" + "<
+tr > < th > Product ID < /th>" + <td>id</td > < /tr> <tr><th>List Price</th > < td > price < /td></tr > < tr > < th > Platform < /th><td>platform</td > < /tr> <tr><th>ESRB Rating</th > < td > esrb < /td></tr > < tr > < th > Condition < /th><td>condition</td > < /tr> <tr><th>Release</th > < td > release < /td></tr >
+    <
+    /table>
+summary
+w "
