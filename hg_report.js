9972515813ea7228cc5338e3e0adb37502daf07f
Unknown <pcapuano978@west-mec.org>
2019-03-01 15:27:34 -0700

    Project Nearing Completion
    Today, I applied in the first bit of HTML for the main page of the website, then worked on code for the second part, however, it doesn't seem as if I applied it correctly, so that will be done soon enough. Speaking of which, I feel like getting the rest of this project done over the weekend.

--- hg_report.js
+++ hg_report.js
@@ -12,9 +12,40 @@
 	
 */
 
-var gameReport = "<h1>title</h1>" + "<h2>By: manufacturer</h2>" + "<img src='hg_id.png'" + "alt='id'" + "id='gameImg'/>" + "<table>" + "<
-tr > < th > Product ID < /th>" + <td>id</td > < /tr> <tr><th>List Price</th > < td > price < /td></tr > < tr > < th > Platform < /th><td>platform</td > < /tr> <tr><th>ESRB Rating</th > < td > esrb < /td></tr > < tr > < th > Condition < /th><td>condition</td > < /tr> <tr><th>Release</th > < td > release < /td></tr >
-    <
-    /table>
-summary
-w "
+//
+var gameReport = "<h1>" + itemTitle + "</h1>" + "<h2>By:" + itemManufacturer + "</h2>" + "<img src='hg_" + itemID + ".png'" + "alt='" + itemID + "'" + "id='gameImg'/>" + "<table>" + "<tr> <th> Product ID </th>" + "<td>" + itemID + "</td>" + "</tr>" + "<tr><th>List Price</th>" + "<td>" + itemPrice + "</td>" + "</tr>" + "<tr> <th> Platform </th>" + "<td>" + itemPlatform + "</td>" + "</tr><tr><th>ESRB Rating</th>" + "<td>" + itemESRB + "</td>" + "</tr>" + "<tr> <th> Condition </th>" + "<td>" + itemCondition + "</td>" + "</tr>" + "<tr><th>Release</th>" + "<td>" + itemRelease + "</td>" + "</tr>" + "</table>" + itemSummary;
+
+//
+document.getElementsByTagName("article")[0].innerHTML = gameReport;
+
+//
+var ratingsSum = 0;
+
+//
+var ratingsCount = ratings[i].length;
+
+
+//
+for (var i; i <= ratings[i].length; i++) {
+    ratingsCount += ratingsSum;
+}
+
+//
+var ratingsAvg = ratingsSum % ratingsCount;
+
+//
+ratingReport = "<h1>Customer Reviews</h1>" + "<h2>" + ratingsAvg + "out of 5 stars (count reviews)</h2>"
+
+//
+for (var i; i <= 2; i++) {
+    ratingReport += "div class = 'review'> <h1>" + ratingTitle + "</h1>" + "<table> <tr> <th> By </th>" + "<td>" + ratingAuthor + "</td>" + "</tr>" + "<tr><th>" + "Review Date</th>" + "<td>" + ratingDates + "</td>" + "</tr>" + "<tr> <th>  Rating</th>" + "<td>";
+
+    for (var i; i < ratingReport; i++) {
+        ratingReport += "<img src='hg_star.png'/>";
+    }
+
+    ratingReport +=
+        "</td>" + "</tr>" + "</table>" + ratingSummaries + "</div>";
+}
+
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
